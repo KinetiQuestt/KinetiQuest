af240c815644cf0483fd887f72436fc8d0a463db
Jackb18b <jackb18b@gmail.com>
2024-10-16 15:01:39 -0400

    Added timing tracking to User class

--- app/models.py
+++ app/models.py
@@ -1,12 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 db = SQLAlchemy()
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
+
+    # Timing Columns
+    account_creation = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    account_updated = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+    most_recent_login = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    # Pet Tracking Columns - not quite sure how long its gonna
+    # stay like this, kinda want
+    pet_name = db.Column(db.String(80))
+
+    # Representation of User (can be more flushed out)
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}), name={self.username!r}"
 
 class Quest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
